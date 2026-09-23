55d413f215a322046d9287af17a57858f8972761
lucindajosie <122471978+lucindajosie@users.noreply.github.com>
2026-09-23 01:54:05 -0400

    sudoku solver code

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,47 @@
 from z3 import *
 
-x = Int("x")
+cells = [[Int(f'cell_{r}_{c}') for c in range(9)] for r in range(9)]
 
-solver = Solver()
+s = Solver()
 
-solver.add(x > 0, x < 10)
+for r in range(9):
+    for c in range(9):
+        s.add(And(cells[r][c] >= 1, cells[r][c] <= 9))
 
-if solver.check() == sat:
-    model = solver.model()
-    print(model.evaluate(x))
+for r in range(9):
+    s.add(Distinct(cells[r]))
+
+for c in range(9):
+    column = [cells[r][c] for r in range(9)]
+    s.add(Distinct(column))
+
+for r in range(0, 9, 3):
+    for c in range(0, 9, 3):
+        box = [cells[i][j] for i in range(r, r + 3) for j in range(c, c + 3)]
+        s.add(Distinct(box))
+
+puzzle = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+for r in range(9):
+    for c in range(9):
+        if puzzle[r][c] != 0:
+            s.add(cells[r][c] == puzzle[r][c])
+
+if s.check() == sat:
+    m = s.model()
+
+    for r in range(9):
+        row = [m.evaluate(cells[r][c]) for c in range(9)]
+        print(row)
+else:
+    print("No solution exists")
